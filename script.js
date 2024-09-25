700bf0ee7b55a343ca593d24b9e5242ae0e4d44c
macarenacarrica <macarenacarrica23@gmail.com>
2024-09-25 18:53:15 -0300

    referencia al script y filtro

--- script.js
+++ script.js
@@ -37,6 +37,12 @@ function showList(array) {
 }
 
 document.addEventListener("DOMContentLoaded", (e) => {
-  // Escribe tu solución aquí
-  // Sugerencia de cómo mostrar el array => showList(strangeArray);
+ // Filtrar elementos que son solo strings
+ const stringsArray = strangeArray.filter(element => typeof element === 'string');
+
+ // Ordenar alfabéticamente
+ stringsArray.sort();
+
+ // Mostrar la lista
+ showList(stringsArray);
 });
